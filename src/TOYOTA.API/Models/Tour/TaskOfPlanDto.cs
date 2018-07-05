8cd2ee802fdbdfebf28895aa5decf61950cd148a
程翠翠(북경개발센터 정보시스템실) <Cheng.CuiCui@home.e-kmall.com>
2018-07-05 21:09:39 +0800

    add tcno & scoreyn

--- src/TOYOTA.API/Models/Tour/TaskOfPlanDto.cs
+++ src/TOYOTA.API/Models/Tour/TaskOfPlanDto.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TOYOTA.API.Models.Tour
+﻿namespace TOYOTA.API.Models.Tour
 {
     public class TaskOfPlanDto
     {
         public int TPId { get; set; }
         public string TCCode { get; set; }
+        public string TCNo { get; set; }
         public string TCTitle { get; set; }
         public string TPStatus { get; set; }
         public string TPType { get; set; }
         public string SourceType { get; set; }
         public string PTitle { get; set; }
         public string UserName { get; set; }
+        public string ScoreYN { get; set; }
     }
 }
